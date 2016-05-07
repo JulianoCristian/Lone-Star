c320cb12ad9e4e19df12e9a6262bef096aea887d
kishkumen123 <rizik944@gmail.com>
2016-05-07 02:35:10 -0500

    fixed small rotation issue

--- Assets/scripts/destroy.cs
+++ Assets/scripts/destroy.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class destroy : MonoBehaviour {
 
+	public float distroy_time;
 	// Use this for initialization
 	void Start () {
-		Destroy(gameObject, 8f);
+		Destroy(gameObject, distroy_time);
 	}
 	
 	// Update is called once per frame
