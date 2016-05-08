794f33c742e2e9bb64d1b9b79d5f103b00d486f8
kishkumen123 <rizik944@gmail.com>
2016-05-07 22:49:21 -0500

    added initials of enemies and death print outs

---
+++ Assets/scripts/death.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "enemy"){
+			print("i am dead");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y < 10){
+			print("i am dead");
+		}
+	}
+}
---
+++ Assets/scripts/enemy_movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_movement : MonoBehaviour {
+
+    public float speed = 15f;
+
+    private Rigidbody body;
+    private Transform target;
+    
+
+
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        Vector3 displacement_from_target = target.position - transform.position;
+        Vector3 direction_to_target = displacement_from_target.normalized;
+        Vector3 velocity = direction_to_target * speed;
+        
+        body.AddForce(velocity);
+        //transform.Translate(velocity * Time.deltaTime);
+	}
+}
