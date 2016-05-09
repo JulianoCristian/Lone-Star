d18c7f85062b48ed82f32e7d16b84e7d78db5efa
kishkumen123 <rizik944@gmail.com>
2016-05-09 16:09:42 -0500

    just pulling

--- Assets/scripts/audio_play.cs
+++ Assets/scripts/audio_play.cs
@@ -5,8 +5,6 @@ public class audio_play : MonoBehaviour {
 
 
     private AudioSource fire_sound;
-    private bool decrement_volume = false;
-    private double vol;
     public float fade_time;
 
 	void Start () {
@@ -14,7 +12,7 @@ public class audio_play : MonoBehaviour {
         fade_time = 0.3f;
 	}
 	
-    IEnumerator _fade_sound(){
+    IEnumerator fade_sound(){
         float t = fade_time;
         while(t > 0 && !Input.GetButton("Fire1")){
             yield return null;
@@ -26,25 +24,15 @@ public class audio_play : MonoBehaviour {
 
 	void Update () {
         if(Input.GetButton("Fire1")){
-            StopCoroutine(_fade_sound());
+            StopCoroutine(fade_sound());
             fire_sound.volume = fade_time;
             if(!fire_sound.isPlaying){
                 fire_sound.Play();
             }
         }
         if(Input.GetButtonUp("Fire1")){
-            StartCoroutine(_fade_sound());
+            StartCoroutine(fade_sound());
             fire_sound.volume = fade_time;
         }
-       // if(decrement_volume){
-       //     vol = vol - 0.1;
-       //     fire_sound.volume = vol;
-       // }
-       // if (fire_sound.volume == 0){
-       //     fire_sound.Stop();
-       //     fire_sound.volume = 0.3;
-       //     vol = fire_sound.volume;
-       //     decrement_volume = false;
-       // }
 	}
 }
--- Assets/scripts/enemy_lives.cs
+++ Assets/scripts/enemy_lives.cs
@@ -5,14 +5,31 @@ public class enemy_lives : MonoBehaviour {
 
     public int life_total;
     private int life;
+    private bool dead = false;
 
+    private AudioSource death_sound;
+    public float fade_time;
 
 	void Start () {
+        death_sound = GetComponent<AudioSource>();
 	    life = life_total;
+        fade_time = 0.3f;
+
 	}
+
+    IEnumerator fade_sound(){
+        float t = fade_time;
+        while(t > 0){
+            yield return null;
+            t -= Time.deltaTime;
+            death_sound.volume = t;
+        }
+        yield break;
+    }
 
     void OnEnable(){
         life = life_total;
+        dead = false;
     }
 
     public void life_subtract(){
@@ -20,7 +37,12 @@ public class enemy_lives : MonoBehaviour {
     }
 	
 	void Update () {
-        if(life <= 0){
+        if(life <= 0 && !dead){
+            dead = true;
+            death_sound.Play();
+            //StartCoroutine(fade_sound());
+        }
+        if(dead && !death_sound.isPlaying){
             gameObject.SetActive(false);
         }
 	}
